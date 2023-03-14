d8064cd28cf4a1ded97cce9634013f0af33c3803
choideakook <shdrnrhd113@naver.com>
2023-03-14 10:53:35 +0900

    230314 접속 test 완료

--- src/test/java/com/ll/basic1/algorithm/AlgorithmTest.java
+++ src/test/java/com/ll/basic1/algorithm/AlgorithmTest.java
@@ -2,17 +2,25 @@ package com.ll.basic1.algorithm;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class AlgorithmTest {
 
     @Test
     void t1() {
         int n = 15;
-        int answer = 4;
+        int answer = 0;
         //--------------------------------//
+        String num = Integer.toString(n, 2);
+        int length = num.replace("0", "").length();
 
+        for (int i = n + 1; i > n; i++) {
+            String s = Integer.toString(i, 2);
+            int iLength = s.replace("0", "").length();
 
+            if (length == iLength) {
+                answer = i;
+                break;
+            }
+        }
 
 
 //        for (int i : answer)
